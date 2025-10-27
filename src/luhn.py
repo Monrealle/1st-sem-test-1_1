d5e8d7d7c6e5aa004bcd3bbcdc49fa1e59ca6682
Stanislav Klimovich <stasklimovish@gmail.com>
2025-10-27 16:41:23 +0300

    Luhn.py has been updated. Comments have been added to the functions and to the code, cardNumber has been changed to number_card, the main() function has been added, and an entry point has been added at the end of the code

--- src/luhn.py
+++ src/luhn.py
@@ -1,8 +1,21 @@
-def luhnСheck(cardNumber):
-    digits = [int(d) for d in str(cardNumber) if d.isdigit()]
-    control = digits.pop()
-    parity = (len(digits))%2
+def luhnСheck(number_card):
+    """
+    Проверяет ввод по алгоритму Луна
+
+    Args:
+        numbee_card: Номер карты в виде строки или числа
+                     Может содержать пробелы и другие символы
+    Return:
+        True: Если номер карты корректен
+        False: Если номер карты неправильный
+    """
+    # Убираем все нецифровые символы
+    digits = [int(d) for d in str(number_card) if d.isdigit()]
+
+    control = digits.pop() # Контрольная цифра
+    parity = (len(digits)) % 2
     total = 0
+
     for i in range(len(digits)):
         if i % 2 == parity:
             doubled = digits[i] * 2
@@ -11,4 +24,36 @@ def luhnСheck(cardNumber):
             total += doubled
         else:
             total += digits[i]
+
     return (total + control) % 10 == 0
+
+def main():
+    """Консольная утилита на основе алгоритма Луна для проверки номеров карт"""
+    print("Введите номер карты или -1 для выхода:)
+
+    while True:
+        try:
+            user_input = input("> ").strip()
+
+            # Проверяем ввод на наличие -1
+            if user_input == '-1':
+                print("Выход из программы")
+                break
+
+            # Проверяем, что ввод не пустой
+            if not user_input:
+                print("Ошибка, введите номер карты)
+                continue
+            # Проверяем номер карты
+            if luhnСheck(user_input):
+                print("Correct")
+
+            else:
+                print("Incorrect")
+
+        except KeyboardInterrupt:
+            print("\nВыход из программы")
+            break
+
+if __name__ == "__main__":
+    main()
