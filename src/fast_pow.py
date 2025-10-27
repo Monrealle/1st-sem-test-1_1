adfa95245ecf2caeaf16f601e48bf3926c1daecb
Stanislav Klimovich <stasklimovish@gmail.com>
2025-10-27 16:01:24 +0300

    test_fast_pow.py has been modified and new tests have been added (unit tests, edge cases, and a property based test). fast_pow.py has been fixed

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -7,7 +7,7 @@ def fastPow(number, power):
     result = 1
     base = number
     while power > 0:
-        if power % 2 == 1
+        if power % 2 == 1:
             result *= base
         base *= base
         power = power // 2
--- test/test_fast_pow.py
+++ test/test_fast_pow.py
@@ -1,9 +1,18 @@
 from src.fast_pow import fastPow
 
 
-def test_two_power_two():
+def test_unit_tests(): # unit тесты
     assert fastPow(2, 2) == 4
+    assert fastPow(1, 10) == 1
+    assert fastPow(3, 4) == 81
+    assert fastPow(5, 3) == 125
 
 
-def test_negative():
+def test_extreme_cases(): # Крайние случаи
     assert fastPow(-1, 4) == 1
+    assert fastPow(0, 10) == 0
+    assert fastPow(67, 0) == 1
+    assert fastPow(-2, 3) == -8
+
+def test_property_based_tests(): # Property based тесты
+    assert fastPow(7, 4) == 7 ** 4
