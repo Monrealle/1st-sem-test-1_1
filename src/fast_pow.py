882168756b827cbe8871112f02f4a6233c74cecc
Stanislav Klimovich <stasklimovish@gmail.com>
2025-10-27 15:47:21 +0300

    The fast_pow.py file has been fixed and the handling of negative powers and powers of 0 has been added

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,14 @@
 def fastPow(number, power):
-    result = number
-    while power != 1:
-        result *= result
+    if power < 0:
+        print("Степень не может быть отритцательной")
+    if power == 0:
+        return 1
+
+    result = 1
+    base = number
+    while power > 0:
+        if power % 2 == 1
+            result *= base
+        base *= base
         power = power // 2
     return result
